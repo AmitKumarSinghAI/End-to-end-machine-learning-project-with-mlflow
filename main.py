bcbf2ddc8b670f54322ad20b4d731a397c7069c9
AmitKumarSinghAI <amitkumarsinghkurmi123@gmail.com>
2026-07-20 13:09:49 +0545

    data ingestion added

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
+from mlProject.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeling
 from mlProject import logger
-from pathlib import Path
-from mlProject.utils.common import read_yaml
 
 
-read_yaml(Path("schema.yaml"))
+STAGE_NAME = "Data Ingestion stage"
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    obj = DataIngestionTrainingPipeling()
+    obj.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<<\n\nx============x")
+except Exception as e:
+    logger.exception(e)
+    raise e
